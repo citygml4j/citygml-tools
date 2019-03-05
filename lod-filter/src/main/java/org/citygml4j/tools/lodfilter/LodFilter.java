2ffb046b78985d2c68cfcc9b6738f0bf53ecc96b
Claus Nagel <cnagel@virtualcitysystems.de>
2019-03-05 16:59:13 +0100

    added first version of filter-lods command

--- lod-filter/src/main/java/org/citygml4j/tools/lodfilter/LodFilter.java
+++ lod-filter/src/main/java/org/citygml4j/tools/lodfilter/LodFilter.java
@@ -1,19 +1,177 @@
 package org.citygml4j.tools.lodfilter;
 
+import org.citygml4j.model.citygml.appearance.Appearance;
 import org.citygml4j.model.citygml.core.AbstractCityObject;
+import org.citygml4j.model.citygml.core.LodRepresentation;
+import org.citygml4j.model.common.base.ModelObject;
+import org.citygml4j.model.common.base.ModelObjects;
+import org.citygml4j.model.common.child.Child;
+import org.citygml4j.model.gml.base.AbstractGML;
+import org.citygml4j.model.gml.base.AssociationByRepOrRef;
+import org.citygml4j.util.child.ChildInfo;
 import org.citygml4j.util.walker.FeatureWalker;
 
-public class LodFilter {
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.IdentityHashMap;
+import java.util.List;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
 
-    public AbstractCityObject filterLod(AbstractCityObject cityObject) {
+public class LodFilter {
+    private final boolean[] lods = {false, false, false, false, false};
+    private final AppearanceCleaner appearanceCleaner = new AppearanceCleaner();
+    private final ChildInfo childInfo = new ChildInfo();
+    private final Set<String> removedCityObjects = ConcurrentHashMap.newKeySet();
+
+    private LodFilterMode mode = LodFilterMode.KEEP;
+    private boolean keepCityObjectsWithoutLods;
+    private List<Appearance> globalAppearances;
+
+    public LodFilter filterLod(int lod) {
+        if (lod >= 0 && lod < lods.length)
+            lods[lod] = true;
+
+        return this;
+    }
+
+    public LodFilter withFilterMode(LodFilterMode mode) {
+        this.mode = mode;
+        return this;
+    }
+
+    public LodFilter keepCityObjectsWithoutLods(boolean keepCityObjectsWithoutLods) {
+        this.keepCityObjectsWithoutLods = keepCityObjectsWithoutLods;
+        return this;
+    }
+
+    public LodFilter withGlobalApps(List<Appearance> appearances) {
+        globalAppearances = appearances;
+        return this;
+    }
+
+    public boolean hasRemainingGlobalApps() {
+        return globalAppearances != null && !globalAppearances.isEmpty();
+    }
+
+    public List<Appearance> getRemainingGlobalApps() {
+        return globalAppearances;
+    }
+
+    public Set<String> getRemovedCityObjectIds() {
+        return new HashSet<>(removedCityObjects);
+    }
+
+    public AbstractCityObject apply(AbstractCityObject cityObject) {
+        IdentityHashMap<AbstractCityObject, List<AbstractCityObject>> tree = new IdentityHashMap<>();
+        List<AssociationByRepOrRef<? extends AbstractGML>> removedProperties = new ArrayList<>();
+        boolean[] filterLods = calcFilterLods(cityObject);
 
         cityObject.accept(new FeatureWalker() {
-            public void visit(AbstractCityObject cityObject) {
-                super.visit(cityObject);
+            public void visit(AbstractCityObject nested) {
+                LodRepresentation representation = nested.getLodRepresentation();
+                if (representation != null) {
+                    for (int lod = 0; lod < filterLods.length; lod++) {
+                        if (representation.isSetRepresentation(lod) && (filterLods[lod] ^ mode != LodFilterMode.REMOVE)) {
+                            List<AssociationByRepOrRef<? extends AbstractGML>> properties = representation.getRepresentation(lod);
+                            for (AssociationByRepOrRef<?> property : properties) {
+                                ModelObject parent = property.getParent();
+                                ModelObjects.unsetProperty(parent != null ? parent : nested, property);
+                            }
+
+                            removedProperties.addAll(properties);
+                        }
+                    }
+                }
+
+                if (!keepCityObjectsWithoutLods && nested != cityObject) {
+                    AbstractCityObject parent = childInfo.getParentCityObject(nested);
+                    tree.computeIfAbsent(parent, v -> new ArrayList<>()).add(nested);
+                }
+
+                super.visit(nested);
             }
         });
 
-        return cityObject;
+        boolean remove = false;
+        if (!keepCityObjectsWithoutLods)
+            remove = removeNode(cityObject, tree, true);
+
+        if (!remove || globalAppearances != null) {
+            Set<String> candidates = appearanceCleaner.getCandidateTargets(removedProperties);
+
+            if (!remove)
+                appearanceCleaner.cleanupAppearances(cityObject, candidates);
+
+            if (globalAppearances != null)
+                appearanceCleaner.cleanupAppearances(globalAppearances, candidates);
+        }
+
+        return remove ? null : cityObject;
+    }
+
+    private boolean[] calcFilterLods(AbstractCityObject cityObject) {
+        if (mode == LodFilterMode.KEEP || mode == LodFilterMode.REMOVE)
+            return lods;
+
+        else {
+            boolean[] filterLods = new boolean[5];
+            int[] min = {Integer.MAX_VALUE};
+            int[] max = {-Integer.MAX_VALUE};
+
+            cityObject.accept(new FeatureWalker() {
+                public void visit(AbstractCityObject cityObject) {
+                    LodRepresentation representation = cityObject.getLodRepresentation();
+                    if (representation.hasLodRepresentations()) {
+                        for (int lod = 0; lod < lods.length; lod++) {
+                            if (lods[lod] && representation.isSetRepresentation(lod)) {
+                                if (lod < min[0]) min[0] = lod;
+                                if (lod > max[0]) max[0] = lod;
+                            }
+                        }
+                    }
+                }
+            });
+
+            if (mode == LodFilterMode.MAXIMUM && max[0] != -Integer.MAX_VALUE)
+                filterLods[max[0]] = true;
+
+            if (mode == LodFilterMode.MINIMUM && min[0] != Integer.MAX_VALUE)
+                filterLods[min[0]] = true;
+
+            return filterLods;
+        }
+    }
+
+    private boolean removeNode(AbstractCityObject node, IdentityHashMap<AbstractCityObject, List<AbstractCityObject>> tree, boolean isParent) {
+        boolean hasChildren = false;
+
+        List<AbstractCityObject> children = tree.get(node);
+        if (children != null) {
+            for (AbstractCityObject child : children) {
+                boolean remove = removeNode(child, tree, false);
+                if (!remove)
+                    hasChildren = true;
+            }
+        }
+
+        if (!hasChildren) {
+            LodRepresentation representation = node.getLodRepresentation();
+            if (!representation.hasLodRepresentations()) {
+                if (!isParent) {
+                    ModelObject property = node.getParent();
+                    if (property instanceof Child)
+                        ModelObjects.unsetProperty(((Child) property).getParent(), property);
+                }
+
+                if (node.isSetId())
+                    removedCityObjects.add(node.getId());
+
+                return true;
+            }
+        }
+
+        return false;
     }
 
 }
--- src/main/java/org/citygml4j/tools/command/FilterLodsCommand.java
+++ src/main/java/org/citygml4j/tools/command/FilterLodsCommand.java
@@ -2,10 +2,13 @@ package org.citygml4j.tools.command;
 
 import org.citygml4j.builder.jaxb.CityGMLBuilderException;
 import org.citygml4j.model.citygml.CityGML;
+import org.citygml4j.model.citygml.appearance.Appearance;
+import org.citygml4j.model.citygml.cityobjectgroup.CityObjectGroup;
 import org.citygml4j.model.citygml.core.AbstractCityObject;
 import org.citygml4j.model.citygml.core.CityModel;
 import org.citygml4j.model.gml.feature.AbstractFeature;
 import org.citygml4j.tools.common.helper.CityModelInfoReader;
+import org.citygml4j.tools.common.helper.GlobalAppReader;
 import org.citygml4j.tools.common.log.Logger;
 import org.citygml4j.tools.lodfilter.LodFilter;
 import org.citygml4j.tools.lodfilter.LodFilterMode;
@@ -21,6 +24,7 @@ import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import java.util.UUID;
 
 @CommandLine.Command(name = "filter-lods",
@@ -31,8 +35,11 @@ public class FilterLodsCommand implements CityGMLTool {
     @CommandLine.Option(names = "--lod", paramLabel = "<lod>", required = true, split = ",", description = "LoD to filter: 0, 1, 2, 3, 4.")
     private List<Integer> lods;
 
-    @CommandLine.Option(names = "--lod-mode", description = "Filter mode: ${COMPLETION-CANDIDATES}.")
-    private LodFilterMode mode;
+    @CommandLine.Option(names = "--mode", description = "Filter mode: ${COMPLETION-CANDIDATES} (default: ${DEFAULT-VALUE}).")
+    private LodFilterMode mode = LodFilterMode.KEEP;
+
+    @CommandLine.Option(names = "--keep-cityobjects-without-lods", description = "Do not delete city objects that lack an LoD representation after filtering.")
+    private boolean keepCityObjectsWithoutLods;
 
     @CommandLine.Option(names = "--overwrite-files", description = "Overwrite input file(s).")
     private boolean overwriteInputFiles;
@@ -51,7 +58,7 @@ public class FilterLodsCommand implements CityGMLTool {
         Logger log = Logger.getInstance();
         String fileNameSuffix = "_filtered_lods";
 
-        LodFilter lodFilter = new LodFilter();
+        GlobalAppReader globalAppReader = new GlobalAppReader(main.getCityGMLBuilder());
 
         log.debug("Searching for CityGML input files.");
         List<Path> inputFiles = new ArrayList<>();
@@ -75,6 +82,23 @@ public class FilterLodsCommand implements CityGMLTool {
                 log.debug("Writing temporary output file '" + outputFile.toAbsolutePath() + "'.");
             }
 
+            List<Appearance> appearances;
+            try {
+                log.debug("Reading global appearances from input file.");
+                appearances = globalAppReader.readGlobalApps(inputFile);
+            } catch (CityGMLBuilderException | CityGMLReadException e) {
+                log.error("Failed to read global appearances.", e);
+                return false;
+            }
+
+            LodFilter lodFilter = new LodFilter()
+                    .withFilterMode(mode)
+                    .keepCityObjectsWithoutLods(keepCityObjectsWithoutLods)
+                    .withGlobalApps(appearances);
+
+            for (int lod : lods)
+                lodFilter.filterLod(lod);
+
             log.debug("Reading city objects from input file and filtering LoDs.");
 
             try (CityGMLReader reader = input.createCityGMLReader(inputFile, main.getCityGMLBuilder(), true);
@@ -90,14 +114,39 @@ public class FilterLodsCommand implements CityGMLTool {
                         isInitialized = true;
                     }
 
-                    if (cityGML instanceof AbstractCityObject) {
+                    if (cityGML instanceof CityObjectGroup) {
+                        CityObjectGroup group = (CityObjectGroup) cityGML;
+
+                        if (group.isSetGroupMember() || group.isSetGroupParent()) {
+                            Set<String> ids = lodFilter.getRemovedCityObjectIds();
+
+                            if (group.isSetGroupMember())
+                                group.getGroupMember().removeIf(member -> member.isSetHref()
+                                        && ids.contains(member.getHref().replaceAll("^#", "")));
+
+                            if (group.isSetGroupMember())
+                                writer.writeFeatureMember(group);
+                        }
+                    }
+
+                    else if (cityGML instanceof AbstractCityObject) {
                         AbstractCityObject cityObject = (AbstractCityObject) cityGML;
-                        cityObject = lodFilter.filterLod(cityObject);
+                        cityObject = lodFilter.apply(cityObject);
                         if (cityObject != null)
                             writer.writeFeatureMember(cityObject);
-                    } else if (cityGML instanceof AbstractFeature && !(cityGML instanceof CityModel))
+                    }
+
+                    else if (cityGML instanceof AbstractFeature
+                            && !(cityGML instanceof CityModel)
+                            && !(cityGML instanceof Appearance))
                         writer.writeFeatureMember((AbstractFeature) cityGML);
                 }
+
+                if (lodFilter.hasRemainingGlobalApps()) {
+                    for (Appearance appearance : lodFilter.getRemainingGlobalApps())
+                        writer.writeFeatureMember(appearance);
+                }
+
             } catch (CityGMLBuilderException | CityGMLReadException e) {
                 log.error("Failed to read city objects.", e);
                 return false;
