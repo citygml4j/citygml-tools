fca0b24cfaef54bb7a13d1798534742c1bbac4fb
Claus Nagel <cnagel@virtualcitysystems.de>
2022-06-15 08:57:58 +0200

    added application skeleton

---
+++ src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module org.citygml4j.tools {
+    requires org.citygml4j.core;
+    requires org.citygml4j.xml;
+    requires org.citygml4j.cityjson;
+    requires info.picocli;
+}
---
+++ src/main/java/org/citygml4j/tools/CityGMLTools.java
@@ -0,0 +1,8 @@
+package org.citygml4j.tools;
+
+public class CityGMLTools {
+
+    public static void main(String[] args) {
+
+    }
+}
