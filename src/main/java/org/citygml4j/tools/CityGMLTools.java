74a5b68a263c727daa6dbc5e18529f959434d306
Claus Nagel <cnagel@virtualcitysystems.de>
2022-06-18 13:25:21 +0200

    completed first version of main command

--- src/main/java/org/citygml4j/tools/CityGMLTools.java
+++ src/main/java/org/citygml4j/tools/CityGMLTools.java
@@ -1,8 +1,283 @@
+/*
+ * citygml-tools - Collection of tools for processing CityGML files
+ * https://github.com/citygml4j/citygml-tools
+ *
+ * citygml-tools is part of the citygml4j project
+ *
+ * Copyright 2018-2022 Claus Nagel <claus.nagel@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package org.citygml4j.tools;
 
-public class CityGMLTools {
+import org.citygml4j.cityjson.ExtensionLoader;
+import org.citygml4j.core.ade.ADEException;
+import org.citygml4j.core.ade.ADERegistry;
+import org.citygml4j.tools.cli.CliCommand;
+import org.citygml4j.tools.log.LogLevel;
+import org.citygml4j.tools.log.Logger;
+import org.citygml4j.tools.util.PidFile;
+import org.citygml4j.tools.util.URLClassLoader;
+import org.citygml4j.xml.CityGMLADELoader;
+import picocli.CommandLine;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.time.Duration;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.concurrent.Callable;
+import java.util.stream.Stream;
+
+@CommandLine.Command(
+        name = CityGMLTools.APP_NAME,
+        scope = CommandLine.ScopeType.INHERIT,
+        description = "Collection of tools for processing CityGML files.",
+        mixinStandardHelpOptions = true,
+        versionProvider = CityGMLTools.class,
+        showAtFileInUsageHelp = true,
+        sortOptions = false,
+        subcommands = {
+                CommandLine.HelpCommand.class
+        }
+)
+public class CityGMLTools implements Callable<Integer>, CommandLine.IVersionProvider {
+    @CommandLine.Option(names = "--log-level", scope = CommandLine.ScopeType.INHERIT, paramLabel = "<level>",
+            defaultValue = "info", description = "Log level: ${COMPLETION-CANDIDATES} (default: ${DEFAULT-VALUE}).")
+    private LogLevel logLevel;
+
+    @CommandLine.Option(names = "--log-file", scope = CommandLine.ScopeType.INHERIT, paramLabel = "<file>",
+            description = "Write log messages to this file.")
+    private Path logFile;
+
+    @CommandLine.Option(names = "--pid-file", scope = CommandLine.ScopeType.INHERIT, paramLabel = "<file>",
+            description = "Create a file containing the current process ID.")
+    private Path pidFile;
+
+    @CommandLine.Option(names = "--extensions", scope = CommandLine.ScopeType.INHERIT, paramLabel = "<folder>",
+            description = "Load extensions from this folder.")
+    private Path extensionsDir;
+
+    public static final String APP_NAME = "citygml-tools";
+    public static final String EXTENSIONS_DIR = "extensions";
+    public static final Path APP_HOME;
+    public static final Path WORKING_DIR;
+
+    private final Logger log = Logger.getInstance();
+    private String commandLine;
+    private String subCommandName;
+
+    static {
+        String appHomeEnv = System.getenv("APP_HOME");
+        if (appHomeEnv == null) {
+            appHomeEnv = ".";
+        }
+
+        String workingDirEnv = System.getenv("WORKING_DIR");
+        if (workingDirEnv == null) {
+            workingDirEnv = ".";
+        }
+
+        APP_HOME = Paths.get(appHomeEnv).normalize().toAbsolutePath();
+        WORKING_DIR = Paths.get(workingDirEnv).normalize().toAbsolutePath();
+    }
 
     public static void main(String[] args) {
+        CityGMLTools app = new CityGMLTools();
+        try {
+            System.exit(app.execute(args));
+        } catch (Exception e) {
+            app.logException(e);
+            System.exit(CommandLine.ExitCode.SOFTWARE);
+        }
+    }
 
+    private int execute(String[] args) throws Exception {
+        Instant start = Instant.now();
+        int exitCode = CommandLine.ExitCode.SOFTWARE;
+
+        CommandLine cmd = new CommandLine(this)
+                .setCaseInsensitiveEnumValuesAllowed(true)
+                .setAbbreviatedOptionsAllowed(true)
+                .setAbbreviatedSubcommandsAllowed(true)
+                .setExecutionStrategy(new CommandLine.RunAll());
+
+        try {
+            CommandLine.ParseResult parseResult = cmd.parseArgs(args);
+            List<CommandLine> commandLines = parseResult.asCommandLineList();
+
+            // check for help options
+            for (CommandLine commandLine : commandLines) {
+                if (commandLine.isUsageHelpRequested() || commandLine.isVersionHelpRequested()) {
+                    return CommandLine.executeHelpRequest(parseResult);
+                } else if (commandLine.getCommand() instanceof CommandLine.HelpCommand) {
+                    return cmd.getExecutionStrategy().execute(parseResult);
+                }
+            }
+
+            // check for required subcommand
+            if (!parseResult.hasSubcommand()) {
+                throw new CommandLine.ParameterException(cmd, "Missing required subcommand.");
+            }
+
+            // preprocess commands
+            for (CommandLine commandLine : commandLines) {
+                Object command = commandLine.getCommand();
+                if (command instanceof CliCommand) {
+                    ((CliCommand) command).preprocess(commandLine);
+                }
+            }
+
+            // execute commands
+            commandLine = APP_NAME + " " + String.join(" ", args);
+            subCommandName = commandLines.get(1).getCommandName();
+            exitCode = cmd.getExecutionStrategy().execute(parseResult);
+
+            log.info("Total execution time: " + formatElapsedTime(Duration.between(start, Instant.now())) + ".");
+            int warnings = log.getNumberOfWarnings();
+            int errors = log.getNumberOfErrors();
+
+            if (exitCode == 1) {
+                log.warn(APP_NAME + " execution failed.");
+            } else if (errors != 0 || warnings != 0) {
+                log.info(APP_NAME + " finished with " + warnings + " warning(s) and " + errors + " error(s).");
+            } else {
+                log.info(APP_NAME + " successfully completed.");
+            }
+        } catch (CommandLine.ParameterException e) {
+            cmd.getParameterExceptionHandler().handleParseException(e, args);
+            exitCode = CommandLine.ExitCode.USAGE;
+        } catch (Exception e) {
+            logException(e);
+        } finally {
+            log.close();
+        }
+
+        return exitCode;
+    }
+
+    @Override
+    public Integer call() throws Exception {
+        initializeLogging();
+
+        log.info("Starting " + APP_NAME + ".");
+        loadADEExtensions(extensionsDir);
+        createPidFile();
+
+        log.info("Executing command '" + subCommandName + "'.");
+        return 0;
+    }
+
+    private void initializeLogging() throws IOException {
+        log.setLogLevel(logLevel);
+
+        if (logFile != null) {
+            try {
+                if (logFile.getParent() != null && !Files.exists(logFile.getParent())) {
+                    Files.createDirectories(logFile);
+                } else if (Files.isDirectory(logFile)) {
+                    logFile = logFile.resolve(APP_NAME + ".log");
+                }
+
+                log.debug("Writing log messages to " + logFile.toAbsolutePath() + ".");
+                log.setLogFile(logFile).writeToFile("# " + commandLine);
+            } catch (IOException e) {
+                log.error("Failed to create log file " + logFile.toAbsolutePath() + ".");
+                throw e;
+            }
+        }
+    }
+
+    private void loadADEExtensions(Path extensionsDir) throws ADEException, IOException {
+        extensionsDir = extensionsDir != null ?
+                WORKING_DIR.resolve(extensionsDir) :
+                APP_HOME.resolve(EXTENSIONS_DIR);
+
+        if (Files.exists(extensionsDir) && Files.isDirectory(extensionsDir)) {
+            try (URLClassLoader classLoader = new URLClassLoader(Thread.currentThread().getContextClassLoader())) {
+                try (Stream<Path> stream = Files.walk(extensionsDir)
+                        .filter(path -> path.getFileName().toString().toLowerCase().endsWith(".jar"))) {
+                    stream.forEach(classLoader::addPath);
+                }
+
+                if (classLoader.getURLs().length > 0) {
+                    log.info("Loading extensions from " + extensionsDir.toAbsolutePath() + ".");
+
+                    ADERegistry registry = ADERegistry.getInstance();
+                    registry.loadADEs(classLoader);
+
+                    registry.getADELoader(CityGMLADELoader.class).getADEModules().forEach(
+                            module -> log.debug("Loaded CityGML ADE " + module.getNamespaceURI() +
+                                    " for CityGML version " + module.getCityGMLVersion() + "."));
+
+                    registry.getADELoader(ExtensionLoader.class).getExtensions().forEach(
+                            extension -> log.debug("Loaded CityJSON Extension " + extension.getName() +
+                                    " for CityJSON version " + extension.getCityJSONVersion() + "."));
+                }
+            } catch (ADEException | IOException e) {
+                log.error("Failed to load ADE extensions.");
+                throw e;
+            }
+        } else if (this.extensionsDir != null) {
+            log.warn("The ADE extensions folder " + extensionsDir.toAbsolutePath() + " does not exist.");
+        }
+    }
+
+    private void createPidFile() throws IOException {
+        if (pidFile != null) {
+            try {
+                log.debug("Creating PID file at " + pidFile.toAbsolutePath() + ".");
+                PidFile.create(pidFile, true);
+            } catch (IOException e) {
+                log.error("Failed to create PID file.");
+                throw e;
+            }
+        }
+    }
+
+    private void logException(Exception e) {
+        log.error("The following unexpected error occurred during execution.");
+        log.logStackTrace(e);
+        log.warn(APP_NAME + " execution failed.");
+    }
+
+    private String formatElapsedTime(Duration elapsed) {
+        long d = elapsed.toDaysPart();
+        long h = elapsed.toHoursPart();
+        long m = elapsed.toMinutesPart();
+        long s = elapsed.toSecondsPart();
+
+        if (d > 0) {
+            return String.format("%02d d, %02d h, %02d m, %02d s", d, h, m, s);
+        } else if (h > 0) {
+            return String.format("%02d h, %02d m, %02d s", h, m, s);
+        } else if (m > 0) {
+            return String.format("%02d m, %02d s", m, s);
+        } else {
+            return String.format("%02d s", s);
+        }
+    }
+
+    @Override
+    public String[] getVersion() {
+        return new String[]{
+                getClass().getPackage().getImplementationTitle() +
+                        ", version " + getClass().getPackage().getImplementationVersion(),
+                "(C) 2018-" + LocalDate.now().getYear() + " Claus Nagel <claus.nagel@gmail.com>"
+        };
     }
 }
---
+++ src/main/java/org/citygml4j/tools/util/PidFile.java
@@ -0,0 +1,96 @@
+/*
+ * citygml-tools - Collection of tools for processing CityGML files
+ * https://github.com/citygml4j/citygml-tools
+ *
+ * citygml-tools is part of the citygml4j project
+ *
+ * Copyright 2018-2022 Claus Nagel <claus.nagel@gmail.com>
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package org.citygml4j.tools.util;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.StandardOpenOption;
+
+public class PidFile {
+    private final long pid;
+    private final Path path;
+    private final boolean deleteOnExit;
+
+    private PidFile(Path path, boolean deleteOnExit, long pid) {
+        this.path = path;
+        this.deleteOnExit = deleteOnExit;
+        this.pid = pid;
+    }
+
+    public static PidFile create(Path path, boolean deleteOnExit) throws IOException {
+        return create(path, deleteOnExit, ProcessHandle.current().pid());
+    }
+
+    public static PidFile create(Path path, boolean deleteOnExit, long pid) throws IOException {
+        path = path.normalize().toAbsolutePath();
+        Path parent = path.getParent();
+
+        if (parent != null) {
+            if (Files.exists(parent) && !Files.isDirectory(parent)) {
+                throw new IOException(parent + " exists but is not a directory.");
+            }
+
+            if (!Files.exists(parent)) {
+                Files.createDirectories(parent);
+            }
+        }
+
+        if (Files.exists(path) && !Files.isRegularFile(path)) {
+            throw new IOException(path + " exists but is not a regular file.");
+        }
+
+        try (OutputStream stream = Files.newOutputStream(path, StandardOpenOption.CREATE, StandardOpenOption.TRUNCATE_EXISTING)) {
+            stream.write(Long.toString(pid).getBytes(StandardCharsets.UTF_8));
+        }
+
+        if (deleteOnExit) {
+            addShutdownHook(path);
+        }
+
+        return new PidFile(path, deleteOnExit, pid);
+    }
+
+    public long getPid() {
+        return pid;
+    }
+
+    public Path getPath() {
+        return path;
+    }
+
+    public boolean isDeleteOnExit() {
+        return deleteOnExit;
+    }
+
+    private static void addShutdownHook(final Path path) {
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            try {
+                Files.deleteIfExists(path);
+            } catch (IOException e) {
+                throw new RuntimeException("Failed to delete pid file " + path + ".", e);
+            }
+        }));
+    }
+}
