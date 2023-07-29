bd361ad4e7da911c82ed1ded78d878a6869a9960
Claus Nagel <cnagel@virtualcitysystems.de>
2023-07-29 11:01:12 +0200

    use seed to initialize internal ID creator to avoid deterministic IDs

--- src/main/java/org/citygml4j/tools/util/IdCreator.java
+++ src/main/java/org/citygml4j/tools/util/IdCreator.java
@@ -23,15 +23,22 @@ package org.citygml4j.tools.util;
 
 import org.xmlobjects.gml.util.id.DefaultIdCreator;
 
+import java.security.SecureRandom;
 import java.util.UUID;
 
 public class IdCreator implements org.xmlobjects.gml.util.id.IdCreator {
+    private static final String seed;
+
+    static {
+        seed = Long.toUnsignedString(new SecureRandom().nextLong() ^ System.currentTimeMillis());
+    }
+
     private final String prefix = DefaultIdCreator.getInstance().getDefaultPrefix();
     private long index;
 
     @Override
     public String createId() {
-        String id = "citygml-tools-" + index++;
+        String id = "citygml-tools-" + seed + index++;
         return prefix + UUID.nameUUIDFromBytes(id.getBytes());
     }
 }
